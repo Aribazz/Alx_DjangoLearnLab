34d612f53e61ca0fde41fb99d98ff339172cd5a1
Ariyo <ariyoaribass68@gmail.com>
2025-03-02 16:35:01 +0100

    implementing user authentication view

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,6 +4,7 @@ from .models import Books
 from .models import Library
 from django.contrib.auth.forms import UserCreationForm
 from django.url import reverse_lazy
+from django.contrib.auth import login
 
 
 def list_books(request):
@@ -20,4 +21,3 @@ class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "relationship_app/register.html"
-    
