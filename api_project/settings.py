4e2301582f920ee477f2122a8b15e39452665aed
Ariyo <ariyoaribass68@gmail.com>
2025-03-09 00:42:28 +0100

    Setting Up a New Django Project with Django REST Framework

--- api_project/settings.py
+++ api_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d0ly)ulx^axgq97irw7o=*(-r5(*4@pb6!6#c%xxlaons5eqya'
+SECRET_KEY = 'django-insecure-8^l)o9$!v#ga-zk03q8v)y*hcg7j)f5nxl199$@0lun&@g!c7*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
