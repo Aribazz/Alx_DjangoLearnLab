8c06c7b563ecca27bced6e87ee04e60c40ac45b7
Ariyo <ariyoaribass68@gmail.com>
2025-03-08 22:58:57 +0100

    Setting Up a New Django Project with Django REST Framework

--- api_project/api/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Book(models.Model):
-    title =models.CharFied(max_length=255)
-    author=models.CharFied(max_length=255)
-
-    def__str__(self):
-        return self.title
--- api_project/bookshelf/delete.md
+++
@@ -1,3 +0,0 @@
-from bookshelf.models import Book
-book.delete()
-print(Book.objects.all())  # Should return an empty queryset
