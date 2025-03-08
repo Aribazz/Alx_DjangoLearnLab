b00fd5a7deba54b8d564fc79ddc162ddde55becf
Ariyo <ariyoaribass68@gmail.com>
2025-03-08 04:14:18 +0100

    Building Your First API Endpoint with Django REST Framework

---
+++ api_project/relationship_app/models.py
@@ -0,0 +1,57 @@
+from django.db import models
+from django.conf import settings
+# Create your models here.
+# Author Model
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+# Book Model (ForeignKey to Author)
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+  
+    def __str__(self):
+        return self.title
+
+
+# Library Model (ManyToManyField to Book)
+class Library(models.Model):
+    name = models.CharField(max_length=255)
+    books = models.ManyToManyField(Book)
+
+    def __str__(self):
+        return self.name
+
+# Librarian Model (OneToOneField to Library)
+class Librarian(models.Model):
+    name = models.CharField(max_length=255)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class UserProfile(models.Model):
+    ROLE_CHOICE = [
+        ("Admin", "Admin"),
+        ("Librarian", "Librarian"),
+        ("Member", "Member"),
+    ]
+    user =models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    role =models.CharField(max_length=20, choices=ROLE_CHOICE)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
+
+
+    class Meta:
+        permissions = [
+            ('can_add_book', 'can add book'),
+            ('can_change_book', 'can change book'),
+            ('can_delete_book', 'can delete book'),
+        ]
+
+
