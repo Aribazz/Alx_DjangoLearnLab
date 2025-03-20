46abe41d52e577c0d4c71dbccb233622895a2c5f
Ariyo <ariyoaribass68@gmail.com>
2025-03-20 18:07:24 +0100

    Implementing the Blog's User Authentication System

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -11,11 +11,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-# BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 
@@ -132,12 +131,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'blog/static')
-]
+STATICFILES_DIRS = [BASE_DIR / "blog/static"]
+TEMPLATES[0]['DIRS'] = [BASE_DIR / "blog/templates"]
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = BASE_DIR / "media"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
