44d0c4d6d61fdf2a45bbd43891017206812abf7e
Ariyo <ariyoaribass68@gmail.com>
2025-03-24 10:18:59 +0100

    Project Setup and User Authentication for a Social Media API

---
+++ social_media_api/accounts/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+
+class CustomerUser(AbstractUser):
+    bio = models.CharField(max_length=200)
+    profile_picture = models.ImageField(upload_to="profile_pictures/", blank=True, null=True)
+    followers = models.ManyToManyField("self", symmetrical=False, related_name="following")
+
+    def __str__(self):
+        return self.username
